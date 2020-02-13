c8a4275ab01080ad3faba4c86ca1557a3b29c2b6
Lambda_School_Loaner_198 <lambda_school_loaner_198@Lambda-School-Loaner-198s-MacBook-Pro.local>
2020-02-13 13:52:53 -0800

    should be MVP

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code away!
+const express = require("express");
+const server = express();
+const logger = require("./middleware/logger");
+const welcomeRouter = require("./routers/welcome");
+const userRouter = require("./users/userRouter");
+
+server.use(logger());
+
+server.use(express.json());
+
+const port = 4000;
+
+server.use("/", welcomeRouter);
+server.use("/users", userRouter);
+
+server.listen(port, () => {
+  console.log(`\n*** Server listening on ${port}`);
+});
